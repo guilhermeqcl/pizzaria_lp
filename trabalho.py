37b68b7ed2cb01840e769b4565aca33c9c8058cc
Rafael Felipe dos Santos <99187467+rafael1509@users.noreply.github.com>
2022-10-14 08:22:29 -0300

    Create trabalho.py

---
+++ trabalho.py
@@ -0,0 +1,56 @@
+ def cadastrar_ingrediente(ingrediente, dic_ingredientes, tipo_ingrediente):
+    '''
+    cadastra ingredientes do tipo massa, molho, queijo e cobertura
+
+    param dic_ingredientes: dicionário com as chaves massa, molho, queijo e cobertura. Os valores desse dicionário são uma lista com os ingredientes.
+    param ingrediente: ingrediente a ser adicionado
+    param tipo_ingrediente: massa, molho, queijo ou cobertura
+    return: inventario atualizado
+    type dic_ingredientes: dic{list[str]}
+    type tipo_ingrediente: str
+    type ingrediente: str
+    rtype: dic{list[str]}
+    
+    type ingredientes: str
+    
+    
+    '''
+    pass
+
+
+def remover_ingredientes(ingrediente, dic_ingredientes, tipo_ingrediente):
+        '''
+    remove ingredientes do tipo massa, molho, queijo e cobertura
+
+    param dic_ingredientes: dicionário com as chaves massa, molho, queijo e cobertura. Os valores desse dicionário são uma lista com os ingredientes.
+    param ingrediente: ingrediente a ser removido
+    param tipo_ingrediente: massa, molho, queijo ou cobertura
+    return: inventario atualizado
+    type dic_ingredientes: dic{list[str]}
+    type tipo_ingrediente: str
+    type ingrediente: str
+    rtype: dic{list[str]}
+    
+    
+    '''
+    pass
+
+def listar_ingredientes(dic_ingredientes,  tipo_ingrediente):
+    '''
+    lista ingredientes de um tipo
+
+    param dic_ingredientes: dicionário com as chaves massa, molho, queijo e cobertura. Os valores desse dicionário são uma lista com os ingredientes.
+    param tipo_ingrediente: massa, molho, queijo ou cobertura
+    return: lista de ingredientes do tipo selecionado
+    type dic_ingredientes: dic{list[str]}
+    type tipo_ingrediente: str
+    rtype: list[str]
+    '''
+    pass
+
+def montar_pizza():
+    '''
+    escolhe tipos de ingredientes para montar a pizza
+    
+
+    '''
