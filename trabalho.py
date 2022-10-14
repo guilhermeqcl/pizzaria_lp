5a6044a00fc0041de28cc6c5defacfc8ec7121ed
Rafael Felipe dos Santos <99187467+rafael1509@users.noreply.github.com>
2022-10-14 08:49:09 -0300

    Update trabalho.py

--- trabalho.py
+++ trabalho.py
@@ -1,38 +1,38 @@
- def cadastrar_ingrediente(ingrediente, dic_ingredientes, tipo_ingrediente):
+
+ def cadastrar_ingrediente(ingrediente, preco, dic_ingredientes, tipo_ingrediente):
     '''
     cadastra ingredientes do tipo massa, molho, queijo e cobertura
 
-    param dic_ingredientes: dicionário com as chaves massa, molho, queijo e cobertura. Os valores desse dicionário são uma lista com os ingredientes.
+    param dic_ingredientes: dicionário com as chaves massa, molho, queijo e cobertura. Os valores desse dicionário são um dicionário com os ingredientes.
     param ingrediente: ingrediente a ser adicionado
-    param tipo_ingrediente: massa, molho, queijo ou cobertura
+    param preco: preço do ingrediente
+    param tipo_ingrediente: dicionários massa, molho, queijo ou cobertura com as chaves ingredientes e valor é o preço.
     return: inventario atualizado
-    type dic_ingredientes: dic{list[str]}
-    type tipo_ingrediente: str
+    type dic_ingredientes: dic{tipo_ingrediente}
+    type tipo_ingrediente: dic{ingrediente: preco}
     type ingrediente: str
-    rtype: dic{list[str]}
-    
-    type ingredientes: str
-    
-    
+    type preco: float
+    rtype: dic{dic{ingrediente: valor}}
+    type ingredientes: str    
     '''
+
     pass
 
-
 def remover_ingredientes(ingrediente, dic_ingredientes, tipo_ingrediente):
-        '''
+    '''
     remove ingredientes do tipo massa, molho, queijo e cobertura
 
     param dic_ingredientes: dicionário com as chaves massa, molho, queijo e cobertura. Os valores desse dicionário são uma lista com os ingredientes.
     param ingrediente: ingrediente a ser removido
     param tipo_ingrediente: massa, molho, queijo ou cobertura
     return: inventario atualizado
-    type dic_ingredientes: dic{list[str]}
-    type tipo_ingrediente: str
+    type dic_ingredientes: dic{tipo_ingrediente}
+    type tipo_ingrediente: dic{ingrediente: preco}
     type ingrediente: str
-    rtype: dic{list[str]}
-    
-    
+    rtype: dic{dic{ingrediente: valor}}
+    type ingredientes: str    
     '''
+    
     pass
 
 def listar_ingredientes(dic_ingredientes,  tipo_ingrediente):
@@ -51,6 +51,6 @@ def listar_ingredientes(dic_ingredientes,  tipo_ingrediente):
 def montar_pizza():
     '''
     escolhe tipos de ingredientes para montar a pizza
-    
+
 
     '''
